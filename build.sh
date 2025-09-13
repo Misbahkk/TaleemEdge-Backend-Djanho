2b96dec00f84eacb957618db72d46dc9a0a66481
Misbahkk <misbahyousaf00@gmail.com>
2025-09-13 18:31:00 +0500

    correct

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
 python manage.py migrate
 
-
-if [[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsuperuser --no-input
-
+if [ "$CREATE_SUPERUSER" == "True" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --username $CREATE_SUPERUSER_USERNAME \
+        --email $CREATE_SUPERUSER_EMAIL
 fi
