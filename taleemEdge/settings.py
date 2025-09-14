a0da666f401c9dd29c8c05e4e284352fd741e1b9
Misbahkk <misbahyousaf00@gmail.com>
2025-09-14 16:56:01 +0500

    add media

--- taleemEdge/settings.py
+++ taleemEdge/settings.py
@@ -208,7 +208,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 
 # File upload settings
 FILE_UPLOAD_MAX_MEMORY_SIZE = 100 * 1024 * 1024  # 100MB
